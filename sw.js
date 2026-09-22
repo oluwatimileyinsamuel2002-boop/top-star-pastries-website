db1afa5e356a570828dfb8311783b7f92e914487
oluwatimileyinsamuel2002-boop <oluwatimileyinsamuel2002@gmail.com>
2026-09-22 11:15:01 +0100

    Add service worker

---
+++ sw.js
@@ -0,0 +1,26 @@
+const CACHE_NAME = "top-star-pastries-v1";
+
+const FILES_TO_CACHE = [
+  "/",
+  "/index.html",
+  "/styles.css",
+  "/script.js",
+  "/manifest.json",
+  "/assets/top-star-logo.png"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
